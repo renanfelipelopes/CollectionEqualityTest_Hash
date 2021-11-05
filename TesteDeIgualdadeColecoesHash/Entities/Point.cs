25274ad756bec2082951559f1cac5ff783ff40fc
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-04 21:57:55 -0300

    example of use comparation hash structure

---
+++ TesteDeIgualdadeColecoesHash/Entities/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace TesteDeIgualdadeColecoesHash.Entities
+{
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y) : this()
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
---
+++ TesteDeIgualdadeColecoesHash/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TesteDeIgualdadeColecoesHash.Entities;
+
+namespace TesteDeIgualdadeColecoesHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+
+            a.Add(new Product("TV", 3.000));
+            a.Add(new Product("Notebook", 1.200));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1.200);
+            Console.WriteLine(a.Contains(prod));
+
+            Point point = new Point();
+            Console.WriteLine(b.Contains(point));
+        }
+    }
+}
